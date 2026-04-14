285e33211d14ad4e5c09a699f2807c96c1157371
senzo_12 <lhricss2025@gmail.com>
2026-04-14 13:31:27 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 //Crack By Toya
 module.exports = {
-  BOT_TOKEN: "U Token", // Token bot Telegram
-  OWNER_ID: "U id", // ID pemilik bot
+  BOT_TOKEN: "8663898197:AAEkX9H0yzOCFHMyN9-hdZt_ykA_BQcwTfg", // Token bot Telegram
+  OWNER_ID: "8105949422", // ID pemilik bot
   allowedGroupIds: [-1002401306074, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
 };
