12ed312cca35768ce77416fecfda3ea9945fa0a6
senzo_12 <lhricss2025@gmail.com>
2026-04-14 13:29:13 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+//Crack By Toya
+module.exports = {
+  BOT_TOKEN: "U Token", // Token bot Telegram
+  OWNER_ID: "U id", // ID pemilik bot
+  allowedGroupIds: [-1002401306074, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
+};
